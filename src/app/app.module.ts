e4c973ec0a93d138e657ee9335f362e1eb8fa5b7
MarjorieLis <lissethjj1998@gmail.com>
2025-04-07 16:27:34 -0500

    new

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -4,10 +4,11 @@ import { FormsModule } from '@angular/forms';
 import { RouterModule } from '@angular/router';
 import { AppComponent } from './app.component';
 import { routes } from './app.routes';
+import { RegistroComponent } from './components/registro/registro.component';
 
 
 @NgModule({
-    declarations: [AppComponent],
+    declarations: [AppComponent, RegistroComponent],
     imports: [BrowserModule, FormsModule, RouterModule.forRoot(routes)],
     providers: [],
     bootstrap: [AppComponent],
--- src/app/components/registro/registro.component.ts
+++ src/app/components/registro/registro.component.ts
@@ -1,15 +1,36 @@
 import { Component } from '@angular/core';
 import { Router } from '@angular/router';
+import { CommonModule } from '@angular/common';
+import { FormsModule } from '@angular/forms';
 
 @Component({
   selector: 'app-registro',
   templateUrl: './registro.component.html',
-  styleUrls: ['./registro.component.css']  
+  styleUrls: ['./registro.component.css'],
+  standalone: true, // ✅ ¡Importante!
+  imports: [CommonModule, FormsModule] // ✅ Para que funcione [(ngModel)]
 })
 export class RegistroComponent {
+  usuario = {
+    nombreCompleto: '',
+    correoElectronico: '',
+    contrasena: '',
+    confirmarContrasena: '',
+    esCreador: false
+  };
+
   constructor(private router: Router) {}
 
   navegarAInicioSesion() {
     this.router.navigate(['/login']);
+  }
+
+  registrarUsuario() {
+    if (this.usuario.contrasena !== this.usuario.confirmarContrasena) {
+      alert('Las contraseñas no coinciden');
+      return;
+    }
+
+    console.log('Usuario registrado:', this.usuario);
   }
 }
