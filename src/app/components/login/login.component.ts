fbff574ae49186577068a934a47092ac087506d5
Marjorie Jimenez <majimenezjim@uide.edu.ec>
2025-04-11 17:24:13 -0500

    new

--- src/app/components/login/login.component.ts
+++ src/app/components/login/login.component.ts
@@ -1,15 +1,32 @@
 import { Component } from '@angular/core';
 import { Router } from '@angular/router';
+import { CommonModule } from '@angular/common';  // Importa CommonModule
+import { FormsModule } from '@angular/forms';  // Importa FormsModule para ngModel
 
 @Component({
-  selector: 'app-login',  // IMPORTANTE: Este nombre debe coincidir en app.component.html
+  selector: 'app-login',
+  standalone: true,  // Configura el componente como standalone
+  imports: [CommonModule, FormsModule],  // Agrega CommonModule y FormsModule
   templateUrl: './login.component.html',
   styleUrls: ['./login.component.css']
 })
-
 export class LoginComponent {
+  passwordVisible: boolean = false;
+  usuario = {
+    contrasena: ''
+  };
+
   constructor(private router: Router) {}
+
+  togglePasswordVisibility() {
+    this.passwordVisible = !this.passwordVisible;
+  }
+
   irARegistro() {
     this.router.navigate(['/registro']);
+  }
+
+  onSubmit() {
+    console.log('Formulario enviado', this.usuario);
   }
 }
