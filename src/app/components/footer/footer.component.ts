e747b4f1aa723bad76b1415d00b219a71c955124
Marjorie Jimenez <majimenezjim@uide.edu.ec>
2025-04-11 17:44:14 -0500

    new

--- src/app/components/footer/footer.component.ts
+++ src/app/components/footer/footer.component.ts
@@ -1,15 +1,36 @@
 import { Component } from '@angular/core';
 import { Router } from '@angular/router';
+import { CommonModule } from '@angular/common';
 
 @Component({
   selector: 'app-footer',
+  standalone: true, // Si tu componente es standalone, asegúrate de tener esto
+  imports: [CommonModule],
   templateUrl: './footer.component.html',
   styleUrls: ['./footer.component.css']
 })
 export class FooterComponent {
+  idiomaActual = 'Español';
+  mostrarIdiomas = false;
+
   constructor(private router: Router) {}
 
   irARegistro() {
     this.router.navigate(['/registro']);
+  }
+
+  toggleIdiomaDropdown() {
+    this.mostrarIdiomas = !this.mostrarIdiomas;
+  }
+
+  cambiarIdioma(idioma: string) {
+    this.idiomaActual = idioma;
+    this.mostrarIdiomas = false;
+  }
+
+  cerrarDropdown() {
+    setTimeout(() => {
+      this.mostrarIdiomas = false;
+    }, 200);
   }
 }
